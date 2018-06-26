4b94f900374b9c0ee61efd675a46d30f24e13d60
Gogo-Pupkov <Gogo.Pupkov@gmail.com>
2018-06-26 11:07:36 -0400

    added comments to the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,16 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
+
+# resource template mongodb.repo used to configure mongodb yum repository
 template '/etc/yum.repos.d/mongodb-org-3.6.repo' do
   source 'mongodb.repo.erb'
 end
 
+# resource package mongodb-org used to install mongodb service
 package 'mongodb-org'
 
+# resource service mongod used to enable and start the service
 service 'mongod' do
   supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
